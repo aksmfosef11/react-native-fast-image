27921da6730409444483421129b0d4a266ee149f
lsj <sangjun.lee@nagiji.com>
2020-03-02 17:08:00 +0900

    modify disk cache size

--- android/src/main/java/com/dylanvann/fastimage/FastImageGlideModule.java
+++ android/src/main/java/com/dylanvann/fastimage/FastImageGlideModule.java
@@ -6,4 +6,9 @@ import com.bumptech.glide.module.AppGlideModule;
 // We need an AppGlideModule to be present for progress events to work.
 @GlideModule
 public final class FastImageGlideModule extends AppGlideModule {
+  @Override
+    public void applyOptions(Context context, GlideBuilder builder) {
+        int diskCacheSizeBytes = 1024 * 1024 * 8; // 100 MB
+        builder.setDiskCache(new InternalCacheDiskCacheFactory(context, diskCacheSizeBytes));
+    }
 }
